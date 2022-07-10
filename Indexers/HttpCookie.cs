9efebadeaeb88b39ab315aca392a880b3fbe68d9
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-10 14:11:46 +0200

    Add project files.

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,22 @@
+﻿//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205248#content
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary; // Dictionary<type_of_key, type_of_value> 
+        public DateTime Expiry { get; set; }
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+            set { _dictionary[key] = value; }
+        }
+    }   
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205248#content
+namespace Indexers
+{
+    // if you're working with a class that has a semantic of a collection or a list
+    // or a dictionary you can improve your code by declaring an indexer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Abe";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
